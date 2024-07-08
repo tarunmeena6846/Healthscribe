7f88c424149619f58c3519ee68b7c91df57c1591
tarunmeena6846 <tarunmeena6846@gmail.com>
2024-07-08 16:39:29 +0530

    frontend added with tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-400">hello</h1>
+    </>
+  );
+}
+
+export default App;
